062dc93dcc5200bf92fcf49507666845b80dc09c
Andrew Peck <peckandrew@gmail.com>
2026-03-01 16:55:18 -0800

    lint(top level)

--- resume.el
+++ resume.el
@@ -1,6 +1,6 @@
-;;; resume.el --- package to generate a resume -*- lexical-binding: t; -*-
+;;; resume.el --- Package to generate a resume -*- lexical-binding: t; -*-
 ;
-;; Copyright (C) 2025 Andrew Peck
+;; Copyright (C) 2025-2026 Andrew Peck
 
 ;; Author: Andrew Peck <peckandrew@gmail.com>
 ;; URL: https://github.com/andrewpeck/resume.el
@@ -23,6 +23,21 @@
 ;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
 
 ;;; Commentary:
+;;;
+;; This package provides some simple infrastructure for generating LaTex and
+;; HTML resumes from an elisp datastructure. Quite opinionated and special
+;; purpose (for me) but if you aren't too picky or share my opinions or are
+;; willing to hack a bit it should work for you as well.
+;;
+;; What problem does this solve? I wanted both HTML and PDF versions of the same
+;; resume.
+;;
+;; And of course the best way to do this is in emacs...
+;;
+;; For real examples see:
+;;
+;; - https://andrewpeck.xyz/resume.html
+;; - https://andrewpeck.xyz/resume.pdf
 ;;
 ;;; Code:
 
@@ -30,13 +45,23 @@
 (require 'resume-tex)
 (require 'resume-html)
 
+(defgroup resume nil
+  "Functions to generate resumes from elisp data structures."
+  :group 'resume
+  :link '(url-link "https://github.com/andrewpeck/resume.el")
+  :prefix "resume")
+
 ;;;###autoload
 (defun resume-make (contact-info jobs projects skills project-intro educations)
-  "Generate HTML and Latex resume documents."
-  (resume-make-latex contact-info jobs projects skills project-intro educations)
-  (resume-make-html contact-info jobs projects skills project-intro educations)
+  "Generate HTML and Latex resume documents.
+
+Generate HTML and LaTeX resume documents from provided details.
+
+Takes CONTACT-INFO, JOBS, PROJECTS, SKILLS, PROJECT-INTRO, and EDUCATIONS
+as arguments to create resumes in both formats."
   ;; TODO: copy css file over also
-  )
+  (resume-make-latex contact-info jobs projects skills project-intro educations)
+  (resume-make-html contact-info jobs projects skills project-intro educations))
 
 (provide 'resume)
 ;;; resume.el ends here
